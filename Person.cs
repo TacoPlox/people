d0e3af2df286caaf3688a6253b2da3e1498dd52d
Hiram Hernández <ehiram.hr@gmail.com>
2020-02-19 11:30:24 -0700

    Basic OOP Person example created.

---
+++ Person.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace persons
+{
+    class Person {
+        private string name;
+
+        static public string bloodColor = "red";
+
+        public Person (string name) {
+            this.name = name;
+        }
+
+        public void SayHello () {
+            Console.WriteLine("Hello, my name is " + this.name);
+        }
+
+        static public void PrintRace () {
+            Console.WriteLine("Human");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace persons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person jackSparrow = new Person("Jack Sparrow");
+            jackSparrow.SayHello();
+
+            Person davyJones = new Person("Davy Jones");
+            davyJones.SayHello();
+
+            //Console.WriteLine(jackSparrow.bloodColor);
+            Console.WriteLine(Person.bloodColor);
+
+            Person.PrintRace();
+        }
+    }
+}
